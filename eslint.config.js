54c2d3d0c3cec23ce0a16945b4aed2550bef9ddb
Rintaro Itokawa <me@re-taro.dev>
2024-07-30 11:40:15 +0900

    build: disable top-level-function rule

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,9 @@
 
 import { re_taro } from "@re-taro/eslint-config";
 
-export default re_taro();
+export default re_taro({}, {
+  files: ["src/**/*.ts"],
+  rules: {
+    "re-taro/top-level-function": "off",
+  },
+});
